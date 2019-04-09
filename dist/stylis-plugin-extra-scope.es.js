f18bba933f45c18160e3524afef2a43b23483332
João Ramos <joao.ramos@helppier.com>
2019-04-09 14:36:10 +0100

    Submiting /dist
    https://stackoverflow.com/a/40528869

---
+++ dist/stylis-plugin-extra-scope.es.js
@@ -0,0 +1,16 @@
+function extraScopePlugin(extra) {
+  var scope = extra.trim() + " ";
+  return function (context, content, selectors, parents, line, column, length, type) {
+    if (context !== 2 || type === 107) return;
+
+    for (var i = 0; i < selectors.length; i++) {
+      // https://github.com/Andarist/stylis-plugin-extra-scope/issues/2
+      // block multiple applications of scope
+      if (!selectors[i].includes(scope)) {
+        selectors[i] = "" + scope + selectors[i];
+      }
+    }
+  };
+}
+
+export default extraScopePlugin;
